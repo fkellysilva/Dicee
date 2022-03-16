89bf003824c90588f9f8e5a58c17aec81eae73c6
Francielly Kelly <fransilva.fks@gmail.com>
2022-03-15 22:59:23 -0300

    refactor: use let instead of var

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -17,27 +17,22 @@ class ViewController: UIViewController {
     
     
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        diceeImageViewOne.image = [
-                    UIImage(named: "DiceOne")!,
-                    UIImage(named: "DiceTwo")!,
-                    UIImage(named: "DiceThree")!,
-                    UIImage(named: "DiceFour")!,
-                    UIImage(named: "DiceFive")!,
-                    UIImage(named: "DiceSix")!
-           ][Int.random(in: 0...5)]
        
-        diceeImageViewTwo.image = [
+        let diceeArray = [
             UIImage(named: "DiceOne")!,
             UIImage(named: "DiceTwo")!,
             UIImage(named: "DiceThree")!,
             UIImage(named: "DiceFour")!,
             UIImage(named: "DiceFive")!,
-            UIImage(named: "DiceSix")!
-   ][Int.random(in: 0...5)]
+            UIImage(named: "DiceSix")]
+        
+        diceeImageViewOne.image = diceeArray [Int.random(in: 0...5)]
+       
+        diceeImageViewTwo.image = diceeArray [Int.random(in: 0...5)]
     
-//        or: leftDiceNumber +=1
-     leftDiceNumber = leftDiceNumber + 1
-    rightDiceNumber = rightDiceNumber - 1
+//         leftDiceNumber +=1
+//     leftDiceNumber = leftDiceNumber + 1
+//    rightDiceNumber = rightDiceNumber - 1
     
      
 }
